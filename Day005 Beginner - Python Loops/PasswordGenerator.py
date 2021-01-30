36be95303fb6e555e4b09387248a10966a5e2471
ysai.pro@gmail.com <ysai.pro@gmail.com>
2021-01-30 18:14:34 +0900

    completed Day 5

--- Day005 Beginner - Python Loops/PasswordGenerator.py
+++ Day005 Beginner - Python Loops/PasswordGenerator.py
@@ -24,3 +24,11 @@ for char in range(1,nr_numbers+1):
     password_list.append(random.choice(numbers))
 
 print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is : {password}")
--- Day005 Beginner - Python Loops/README.md
+++ Day005 Beginner - Python Loops/README.md
@@ -7,3 +7,7 @@ for number in range(1,20,3):
 
  #1 4 7 10 13 16 19
 ```
+
+### shuffle list
+
+random.shuffle(list)
